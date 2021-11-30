829d12727ae48e4a46ed61e8d046869a024b4c87
Le Trashman <o12ri3@gmail.com>
2021-11-30 22:21:08 +0000

    Support toggle tiles and maps for event stages

--- data.py
+++ data.py
@@ -1,42 +1,43 @@
 import collections
 import json
-import os
+import pathlib
 
 BlueArchiveData = collections.namedtuple(
     'BlueArchiveData',
     ['campaign_stages', 'campaign_stage_rewards', 'campaign_strategy_objects', 'campaign_units', 'characters',
-     'currencies', 'equipment', 'gacha_elements', 'gacha_elements_recursive', 'gacha_groups', 'items']
+     'currencies', 'equipment', 'event_content_stages', 'gacha_elements', 'gacha_elements_recursive', 'gacha_groups',
+     'items']
 )
 
 
 def load_campaign_stages(path):
-    return load_file(os.path.join(path, 'Excel', 'CampaignStageExcelTable.json'))
+    return load_file(path / 'Excel' / 'CampaignStageExcelTable.json')
 
 
 def load_campaign_stage_rewards(path):
-    return load_file_grouped(os.path.join(path, 'Excel', 'CampaignStageRewardExcelTable.json'), 'GroupId')
+    return load_file_grouped(path / 'Excel' / 'CampaignStageRewardExcelTable.json', 'GroupId')
 
 
 def load_campaign_strategy_objects(path):
-    return load_file(os.path.join(path, 'Excel', 'CampaignStrategyObjectExcelTable.json'))
+    return load_file(path / 'Excel' / 'CampaignStrategyObjectExcelTable.json')
 
 
 def load_campaign_units(path):
-    return load_file(os.path.join(path, 'Excel', 'CampaignUnitExcelTable.json'))
+    return load_file(path / 'Excel' / 'CampaignUnitExcelTable.json')
 
 
 def load_characters(path):
     # TODO: find something better to use as the key
-    return load_file(os.path.join(path, 'Excel', 'CharacterExcelTable.json'),
+    return load_file(path / 'Excel' / 'CharacterExcelTable.json',
                      key='ModelPrefabName',
                      pred=lambda item: item['ProductionStep'] == 'Release')
 
 
 def load_currencies(path):
-    return load_file(os.path.join(path, 'Excel', 'CurrencyExcelTable.json'), key='ID')
+    return load_file(path / 'Excel' / 'CurrencyExcelTable.json', key='ID')
 
 
-def load_data(path):
+def _load_data(path):
     return BlueArchiveData(
         campaign_stages=load_campaign_stages(path),
         campaign_stage_rewards=load_campaign_stage_rewards(path),
@@ -45,6 +46,7 @@ def load_data(path):
         characters=load_characters(path),
         currencies=load_currencies(path),
         equipment=load_equipment(path),
+        event_content_stages=load_event_content_stages(path),
         gacha_elements=load_gacha_elements(path),
         gacha_elements_recursive=load_gacha_elements_recursive(path),
         gacha_groups=load_gacha_groups(path),
@@ -52,21 +54,25 @@ def load_data(path):
     )
 
 
+def load_data(path):
+    return _load_data(pathlib.Path(path))
+
+
 def load_equipment(path):
-    return load_file(os.path.join(path, 'Excel', 'EquipmentExcelTable.json'))
+    return load_file(path / 'Excel' / 'EquipmentExcelTable.json')
+
+
+def load_event_content_stages(path):
+    return load_file(path / 'Excel' / 'EventContentStageExcelTable.json')
 
 
 def load_file(file, key='Id', pred=None):
-    with open(file) as f:
-        data = json.load(f)
-
+    data = json.loads(file.read_bytes())
     return {item[key]: item for item in data['DataList'] if not pred or pred(item)}
 
 
 def load_file_grouped(file, key):
-    with open(file) as f:
-        data = json.load(f)
-
+    data = json.loads(file.read_bytes())
     groups = collections.defaultdict(list)
     for item in data['DataList']:
         groups[item[key]].append(item)
@@ -75,16 +81,16 @@ def load_file_grouped(file, key):
 
 
 def load_gacha_elements(path):
-    return load_file_grouped(os.path.join(path, 'Excel', 'GachaElementExcelTable.json'), 'GachaGroupID')
+    return load_file_grouped(path / 'Excel' / 'GachaElementExcelTable.json', 'GachaGroupID')
 
 
 def load_gacha_elements_recursive(path):
-    return load_file_grouped(os.path.join(path, 'Excel', 'GachaElementRecursiveExcelTable.json'), 'GachaGroupID')
+    return load_file_grouped(path / 'Excel' / 'GachaElementRecursiveExcelTable.json', 'GachaGroupID')
 
 
 def load_gacha_groups(path):
-    return load_file(os.path.join(path, 'Excel', 'GachaGroupExcelTable.json'), key='ID')
+    return load_file(path / 'Excel' / 'GachaGroupExcelTable.json', key='ID')
 
 
 def load_items(path):
-    return load_file(os.path.join(path, 'Excel', 'ItemExcelTable.json'))
+    return load_file(path / 'Excel' / 'ItemExcelTable.json')
